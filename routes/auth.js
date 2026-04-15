86452a841a71707779e9fa66a8f5f4d4c9bd9dae
Juan Jose Restrepo Cuadros <programador-web-global@opa.com.co>
2026-04-15 14:07:15 -0500

    feat(other): ajustes realizados en la autenticacion

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,43 @@
-const express = require('express');
-const { validateUser, login, createUser, recoverUser } = require('../controllers/auth/auth.controller');
+const express = require("express");
+const {
+  validateUser,
+  login,
+  createUser,
+  recoverUser,
+  recoverPasswordCode,
+  recoverPassword,
+  recoverPasswordQuestions
+} = require("../controllers/auth/auth.controller");
 const router = express.Router();
 
 const TITLE_APP = "FONDOOPA | Servicios en Linea";
 
 router.get("/login", (_, res) => {
-    res.render("auth/login/index", {
-        title: TITLE_APP,
-        layout: "layouts/auth"
-    });
+  res.render("auth/login/index", {
+    title: TITLE_APP,
+    layout: "layouts/auth",
+  });
 });
 
 router.get("/register", (_, res) => {
-    res.render("auth/register/index", {
-        title: TITLE_APP,
-        layout: "layouts/auth"
-    });
+  res.render("auth/register/index", {
+    title: TITLE_APP,
+    layout: "layouts/auth",
+  });
 });
 
 router.get("/recover-user", (_, res) => {
-    res.render("auth/recover-user/index", {
-        title: TITLE_APP,
-        layout: "layouts/auth"
-    });
+  res.render("auth/recover-user/index", {
+    title: TITLE_APP,
+    layout: "layouts/auth",
+  });
 });
 
 router.get("/recover-password", (_, res) => {
-    res.render("auth/recover-password/index", {
-        title: TITLE_APP,
-        layout: "layouts/auth"
-    });
+  res.render("auth/recover-password/index", {
+    title: TITLE_APP,
+    layout: "layouts/auth",
+  });
 });
 
 router.post("/validateUser", validateUser);
@@ -39,5 +47,11 @@ router.post("/", login);
 router.post("/register", createUser);
 
 router.post("/recover-user", recoverUser);
+
+router.post("/recover-password", recoverPassword);
+
+router.post("/recover-password/code", recoverPasswordCode);
+
+router.post("/recover-password/questions", recoverPasswordQuestions);
 
 module.exports = router;
